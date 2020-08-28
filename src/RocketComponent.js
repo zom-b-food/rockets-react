eded2e4f879745223636e3db14248024059aa1b2
zom.b.food <am@ui-design-engineering.com>
2020-08-27 17:33:13 -0700

    responsive table fix

--- src/RocketComponent.js
+++ src/RocketComponent.js
@@ -32,44 +32,32 @@ class RocketComponent extends Component {
       
         return (
             <div className="wrapper">
-                <div className="row">
-                    <div className="col-3 first ships"><h5 className="sort">Ships</h5></div>
-                    <div className="col-3 second cores"><h5 className="sort">Cores</h5></div>
-                    <div className="col-3 third links"><h5 className="sort">Links</h5></div>
-                    <div className="col-3 tags"><h5 className="sort">Tags</h5></div>
-                </div>
-                <div className="row">
-                    <div className="col-3 first"><Ships></Ships></div>
-                    <div className="col-3 second"><Cores></Cores></div>
-                    <div className="col-3 third"><Links></Links></div>
-                    <div className="col-3"><General></General></div>
-                </div> 
-            <br /><br />
+               
                <div className="container-fluid">
                 <div className="row">
                   <div className="col-sm">
-                    <div className="first ships">
-                      <h5 className="sort">Ships</h5>
+                    <div className="header">
+                      <h5>Ships</h5>
                     </div>
-                      <div><Ships></Ships></div>
+                    <div className="data"><Ships></Ships></div>
                   </div>               
                   <div className="col-sm">
-                    <div className="second cores">
-                      <h5 className="sort">Cores</h5>
+                    <div className="header">
+                      <h5>Cores</h5>
                     </div>
-                      <div><Cores></Cores></div>
+                    <div className="data"><Cores></Cores></div>
                   </div>
                   <div className="col-sm scrolling">
-                    <div className="third links">
-                      <h5 className="sort">Links</h5>
+                    <div className="header">
+                      <h5>Links</h5>
                     </div>
-                      <div><Links></Links></div>
+                    <div className="data"><Links></Links></div>
                   </div>
                   <div className="col-sm">
-                    <div className="tags">
-                      <h5 className="sort">Tags</h5>
+                    <div className="header">
+                      <h5>Tags</h5>
                     </div>
-                      <div><General></General></div>
+                    <div className="data"><General></General></div>
                   </div>               
                  </div>             
               </div>
